ccdb3ba46a41e7915e4d7a5fc23e1adda9ef9265
app-stante <app.stante@proton.me>
2026-05-06 14:03:54 +0300

    Update app.js

--- Demo/app.js
+++ Demo/app.js
@@ -1,1 +1,20 @@
-// APP JS
+// ======================================================
+// GESTIUNE STANTE - APP JS
+// ======================================================
+
+
+// ===== CONECTARE SUPABASE =====
+
+const SUPABASE_URL = "https://ulgbamusntxfpuagmfwv.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_pWwpM0_vC0PWxunbTjBeUA_KfdVsr4k";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
+
+
+// ===== TEST CONECTARE =====
+
+console.log("Aplicatia este conectata la Supabase");
